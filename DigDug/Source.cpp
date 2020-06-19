475ae50cec89f2fbd322e33a45c7a55e090035f2
Cameron Collins <cameroncollins19@yahoo.com>
2020-06-19 10:16:05 -0500

    Added Project, SDL Directories

---
+++ DigDug/Source.cpp
@@ -0,0 +1,18 @@
+#include <SDL.h>
+
+
+int main(int argc, char* argv[]) {
+
+	//Initialize SDL
+	SDL_Init(SDL_INIT_VIDEO);
+
+	//Game Loop 
+	while (true) {
+
+	}
+
+	//Quit SDL
+	SDL_Quit();
+
+	return 0;
+}
